ec46c7b867a2346ed9f161b21731f29b9d4f8112
matheussouzadev <1002matheussouza@gmail.com>
2025-09-25 19:46:37 -0300

    chore: adds initial configuration for prettier, husky, and lint-staged

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="my-5 flex w-screen items-center justify-center bg-custom">
+      <h1 className="rounded-full border bg-slate-100/50 p-2 font-bold uppercase backdrop-blur-sm">
+        Hello world
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
